43aee438988145f12170ed827e9abaeda8bed2c3
Rafał Mandrysz <rafal.mandrysz@gmail.com>
2023-02-21 19:31:43 +0100

    Add screenshot taking and a lot of improvements for final thesis
    Data19 still the one to be chosen for graph

--- Assets/Data/Plot.py
+++ Assets/Data/Plot.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import statistics as stat
 import scipy as sp
+from matplotlib.ticker import MultipleLocator
 
 dataSet = 19
 here = os.path.dirname(os.path.abspath(__file__))
@@ -23,14 +24,12 @@ referenceLines = [line for line in referenceFile.readlines()]
 grouped = {}
 xdata = []
 averages = []
-standardDeviation = []
 standardError = []
 referenceX = []
 referenceY = []
 
 xdataDivided = []
 averagesDivided = []
-standardDeviatonDiv = []
 standardErrorDiv = []
 referenceXdivided = []
 referenceYdivided = []
@@ -48,7 +47,6 @@ for line in lines:
     value = centerDistance
 
     if startingPoint != 0 and key > startingPoint:
-        print("Starting point: {}, key: {}".format(startingPoint, key))
         continue
 
     if key in grouped:
@@ -69,7 +67,6 @@ for line in referenceLines:
 
 # Count Averages
 for group in sortedGrouped.keys():
-    standardDeviation.append(stat.stdev(grouped[group]))
     standardError.append(np.std(grouped[group]) / np.sqrt(np.size(grouped[group])))
     average = stat.mean(grouped[group])
     averages.append(average)
@@ -79,6 +76,7 @@ xdata = [float(x)*groupingSize for x in sortedGrouped.keys()]
 
 averagesMin = min(averages)
 averagesDivided = [x/averagesMin for x in averages]
+standardErrorDiv = [x/averagesMin for x in standardError]
 xdataPoint = 45.0
 xdataDivided = [x/xdataPoint for x in xdata]
 
@@ -87,21 +85,27 @@ referenceYdivided = [x/referenceMin for x in referenceY]
 referencePoint = 60.0
 referenceXdivided = [x/referencePoint for x in referenceX]
 
-plt.subplot(1, 2, 1)
-plt.errorbar(xdata, averages, yerr=standardError, fmt='-o', color='cyan', ecolor='gray', label="Distance from flock center")
-plt.plot(referenceX, referenceY, 'g-+', label="reference")
-plt.xlabel("Odległość Drapieżnika od centroidu stada")
-plt.ylabel("Średnia odległość ")
-plt.xlim(max([max(xdata), max(referenceX)]), min([min(xdata), min(referenceX)]))
+plt.errorbar(xdata, averages, yerr=standardError, fmt='.', color='black', ecolor='gray', label="Distance from flock center", mfc='none')
+plt.xlabel("Odległość Drapieżnika od centroidu stada [Jednostki silnika Unity]")
+plt.ylabel("Średnia odległość owiec od centroidu stada [Jednostki silnika Unity]")
+plt.xlim(max(xdata) + max(groupingSize, 5), min(xdata) - max(groupingSize, 5))
+# plt.xticks(np.arange(max([max(xdata), max(referenceX)]), min([min(xdata), min(referenceX)]), step=groupingSize))
+plt.xticks(np.arange(min(xdata) - max(groupingSize, 5), max(xdata) + max(groupingSize, 5), step=max(groupingSize, 5)))
 plt.grid(True)
+plt.axes().xaxis.set_minor_locator(MultipleLocator(1))
+plt.savefig("FigMeanDistanceSim.png", bbox_inches='tight', pad_inches=0)
 # plt.title("Change in the number of healthy, sick and recovered agents over time\nwith probability of contraction p={} and recovery p={}".format(spreadChance, recoveryChance))
-plt.legend()
+plt.show()
 
-plt.subplot(1, 2, 2)
-plt.plot(xdataDivided, averagesDivided, 'c-*')
-plt.plot(referenceXdivided, referenceYdivided, 'g-+')
-plt.xlim(max([max(xdataDivided), max(referenceXdivided)]), min([min(xdataDivided), min(referenceXdivided)]))
+plt.errorbar(xdataDivided, averagesDivided, yerr=standardErrorDiv, fmt='.', ecolor='gray', label="Dane otrzymane w symulacji", mfc='none', color='black')
+plt.plot(referenceXdivided, referenceYdivided, '^', label="Dane eksperymentalne", mfc='none', ms='6', color='seagreen')
+plt.xlim(max(xdataDivided) + 0.1, min(xdataDivided) - 0.1)
+plt.xticks(np.arange(round(min(xdataDivided) - 0.1, 1), round(max(xdataDivided) + 0.1, 1), step=0.1))
+plt.xlabel("Odległość Drapieżnika od centroidu stada")
+plt.ylabel("Średnia odległość owiec od centroidu stada")
+plt.legend()
 plt.grid(True)
+plt.savefig("FigMeanDistanceSim.png", bbox_inches='tight', pad_inches=0)
 # plt.xticks(range(0, max(velocity) + 26, 25))
 # plt.yticks(range(0, predatorDistance[0] + 1, 50))
 plt.show()
---
+++ Assets/Resources/Scripts/ScreenshotTaker.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotTaker : MonoBehaviour {
+    public int resWidth = 2550; 
+    public int resHeight = 3300;
+    private int screenshotCount = 0;
+    private int skipFrame = 0;
+
+    private string ScreenShotName(int width, int height) {
+        return string.Format("{0}/Screenshots/screen_{1}.png", 
+                             Application.dataPath, 
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-" + screenshotCount));
+    }
+
+    void LateUpdate() {
+    Camera camera = Camera.main;
+        if (Input.GetKeyDown("k")) 
+        {
+            screenshotCount = 6;
+        }
+        if(screenshotCount > 0 && skipFrame != 0)
+        {
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 32);
+            camera.targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGBA32, false);
+            camera.Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            camera.targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            screenshotCount -= 1;
+        }
+        skipFrame = 1 - skipFrame;
+    }
+}
